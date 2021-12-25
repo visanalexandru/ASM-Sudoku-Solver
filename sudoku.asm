9312f1c82c3430668aeca15b634aad4e3a9ac905
visan <alexandruvisan44@yahoo.com>
2021-12-25 11:53:08 +0200

    Implemented backtracking to solve the puzzle. Fixed points are not yet taken in account.

--- sudoku.asm
+++ sudoku.asm
@@ -12,16 +12,23 @@
 
 	process_int_format: .asciz "%d " # Format to read/write int 
 
-	# For each line and column (0-8), mark used numbers (1-9). So used_line[line][number] is 1 if the given number
-	# appears on the given line, and used_column[column][number] is 1 if the given number appears on the given column
+	# For each line, column and square (0-8), mark used numbers (1-9). So used_line[line][number] is 1 if the given number
+	# appears on the given line, used_column[column][number] is 1 if the given number appears on the given column,
+	# and used_square[square][number] is 1 if the given number appears on the given square.
 	used_line: .space 90 
 	used_column: .space 90
+	used_square: .space 90
 
 .text
 	.global main
 
 	main:
-		call read_grid	
+		call read_grid 
+
+		push $0 # Solve from index 0, meaning from the start of the grid
+		call solve
+		pop %ecx # Pop the parameter
+
 		call output_grid
 
 		et_exit: # Program ends here
@@ -203,5 +210,193 @@
 		pop %ecx # Pop column auxiliary variable
 		pop %ebp
 		ret
+	
+	# This function tries to recursively solve the grid. It returns 1 through eax if it has found
+	# a solution to the puzzle, or 0 if a solution does not exist. If a solution exists, the grid
+	# will be modified accordingly. The only parameter is the index of the current cell to modify.
+	solve:
+		# Create the stack frame
+		push %ebp
+		mov %esp, %ebp
+		push %edi # We modify edi, so we store it first
+		push %ebx # We modify ebx, so we store it first
+		push $0 # The line of the current cell  
+		push $0 # The column of the current cell 
+		push $0 # The square of the current cell 
+		push $0 # A pointer to access used_line[line][number]
+		push $0 # A pointer to access used_column[column][number]
+		push $0 # A pointer to access used_square[square][number]
+		
+		# Check if we have finished solving the grid
+		cmp $81, 8(%ebp)
+		jne et_not_finished
+		
+		et_finished: # We have finished solving the puzzle, return 1 through eax
+			mov $1, %eax
+			jmp et_solve_return
+			
+		et_not_finished:
+			mov 8(%ebp), %edx # Move the current index to edx
+			lea grid, %edi
+			cmp $0, (%edi, %edx, 4) # Check if the current cell is fixed
+			je et_not_fixed
+
+			et_fixed: # The current cell is fixed, just skip the current cell, solve for index+1
+				inc %edx # Increment the current index 
+
+				push %edx
+				call solve # Solve for index+1
+				pop %edx
+
+				jmp et_solve_return
+
+			et_not_fixed: # The current cell is not fixed, we need to try all possible numbers  
+				
+				# We first get the line and column of the current cell
+				push 8(%ebp) # Push the index of the current cell
+				call line_and_column
+				pop %edx
+				
+				mov %eax, -12(%ebp)  # Set the line local variable
+				mov %ecx, -16(%ebp)  # Set the column local variable
+
+
+				# We then get the square of the current cell
+				push 8(%ebp) # Push the index of the current cell
+				call square
+				pop %edx
+
+				mov %eax, -20(%ebp) # Set the square local variable
+
+				mov $1,%ecx 
+				et_choice_loop: # Loop through numbers from 1 to 9
+					cmp $10, %ecx # Check if ecx has reached the end
+					je et_choice_loop_end
+					
+					# Check if the current number already exists on this line
+					# So we need to check used_line[line*10+number] 
+					et_check_line:
+						mov -12(%ebp), %eax # Move the current line index into eax
+						xor %edx, %edx # Set edx to zero 
+						mov $10, %ebx # Set ebx to 10 as we will multipy the line index by 10 
+						mul %ebx
+						add %ecx, %eax # eax now holds line*10+number
+
+						lea used_line, %edi
+						lea (%edi, %eax, 1), %eax # Load the pointer to used_line[line][number] to eax 
+						mov %eax, -24(%ebp) # Update the pointer to used_line[line][number]
+						cmpb $1, (%eax) # If the number already exists on the current line, we cannot add it
+						je et_choice_invalid
+
+					# Check if the current number already exists on this column 
+					# So we need to check used_column[column*10+number] 
+					et_check_column:
+						mov -16(%ebp), %eax # Move the current column index into eax
+						xor %edx, %edx # Set edx to zero 
+						mov $10, %ebx # Set ebx to 10 as we will multipy the column index by 10 
+						mul %ebx
+						add %ecx, %eax # eax now holds column*10+number
+
+						lea used_column, %edi
+						lea (%edi, %eax, 1), %eax # Load the pointer to used_column[column][number] to eax 
+						mov %eax, -28(%ebp) # Update the pointer to used_column[column][number]
+						cmpb $1, (%eax) # If the number already exists on the current column, we cannot add it
+						je et_choice_invalid
+					
+					# Check if the current number already exists in this square
+					# So we need to check used_square[square*10+number]
+					et_check_square:
+						mov -20(%ebp), %eax # Move the current square index into eax
+						xor %edx, %edx # Set edx to zero
+						mov $10, %ebx # Set ebx to 10 as we will multiply the square index by 10
+						mul %ebx
+						add %ecx, %eax # eax now holds square*10+number
+
+						lea used_square, %edi
+						lea (%edi, %eax, 1), %eax # Load the pointer to used_square[square][number] to eax 
+						mov %eax, -32(%ebp) # Update the pointer to used_square[column][number]
+						cmpb $1, (%eax) # If the number already exists on the current square, we cannot add it
+						je et_choice_invalid
+
+					# We found a valid choice, update used_line, used_column, used_sqare and the grid
+					et_choice_valid:	
+						# First update the used_line table, using the pointer to used_line[line][number]
+						mov -24(%ebp), %eax
+						movb $1, (%eax) # Set used_line[line][number] to 1
+
+						# Then update the used_column table, using the pointer to used_column[column][number]
+						mov -28(%ebp), %eax
+						movb $1, (%eax) # Set used_column[column][number] to 1
+
+						# Then update the used_square table, using the pointer to used_square[square][number]
+						mov -32(%ebp), %eax
+						movb $1, (%eax) # Set used_square[column][number] to 1
+						
+						# Then update the grid cell
+						lea grid, %edi
+						mov 8(%ebp), %eax # Move the index of the current cell to eax
+						mov %ecx, (%edi, %eax, 4) # Update the current cell with the current choice
+						
+						# Now call solve for index+1
+						mov 8(%ebp), %eax
+						inc %eax # Eax now holds index+1
+
+						push %ecx # Store ecx before the call
+						push %eax # Push index+1 as a parameter
+						call solve # eax now holds 1 if we found a solution
+						pop %ecx # Pop the parameter
+						pop %ecx # Restore ecx
+					
+						# If eax holds 1, we have found a solution to the puzzle, just return
+						cmp $1, %eax
+						je et_solve_return
+
+						# Else we need to restore the previous state
+						# First restore the used_line table 
+						mov -24(%ebp), %eax
+						movb $0, (%eax) # Set used_line[line][number] to 0 
+
+						# Then restore the used_column table 
+						mov -28(%ebp), %eax
+						movb $0, (%eax) # Set used_column[column][number] to 0 
+
+						# Then restore the used_square table 
+						mov -32(%ebp), %eax
+						movb $0, (%eax) # Set used_square[column][number] to 0 
+
+						# Then restore the grid cell
+						lea grid, %edi
+						mov 8(%ebp), %eax # Move the index of the current cell to eax
+						movl $0, (%edi, %eax, 4) # Set the current cell back to 0 
+
+						jmp et_choice_next # Jump to the next number to try
+
+					et_choice_invalid: # The current number is not valid, try the next one
+						jmp et_choice_next
+					
+					et_choice_next:
+					inc %ecx
+					jmp et_choice_loop
+				et_choice_loop_end:
+
+				# If we did not found any solution, return 0 through eax
+				xor %eax, %eax # Set eax to zero
+				jmp et_solve_return
+
+		et_solve_return:
+			# Delete the stack frame
+			pop %ecx # Pop the pointer to used_square[square][number] 
+			pop %ecx # Pop the pointer to used_column[column][number] 
+			pop %ecx # Pop the pointer to used_line[line][number] 
+			pop %ecx # Pop the square
+			pop %ecx # Pop the column 
+			pop %ecx # Pop the line 
+			pop %ebx # Restore ebx
+			pop %edi # Restore edi
+			pop %ebp
+			ret
+
+
+
 
 
